7d07a7788f34543340afe94c9236c256aeaf982c
Achyut Dhungel <praagyadhungel567@gmail.com>
2026-04-02 09:02:58 +0545

    Add rate limiting to the API

--- app/main.py
+++ app/main.py
@@ -1,16 +1,22 @@
 from datetime import datetime
 import random
 import string
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, Request
 from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
 from app import models
 from app.schemas import URLCreate, URLResponse, URLStats
 from app.database import engine, get_db
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address 
+from slowapi.errors import RateLimitExceeded
 
 models.Base.metadata.create_all(bind=engine)
 
+limiter = Limiter(key_func=get_remote_address)
 app = FastAPI()
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 def get_slug(length: int = 6) -> str :
     chars = string.ascii_letters + string.digits
@@ -27,7 +33,8 @@ def root():
     return {"message": "URL Shortener is running"}
 
 @app.post("/shorten", response_model=URLResponse)
-def shorten_url(payload: URLCreate, db: Session = Depends(get_db)):
+@limiter.limit("10/minute")
+def shorten_url(request: Request, payload: URLCreate, db: Session = Depends(get_db)):
     while True:
         slug = get_slug()
         exists = db.query(models.URL).filter(models.URL.slug == slug).first()
@@ -43,7 +50,8 @@ def shorten_url(payload: URLCreate, db: Session = Depends(get_db)):
     return entry
 
 @app.get("/stats/{slug}", response_model=URLStats)
-def get_stats(slug: str, db: Session = Depends(get_db)):
+@limiter.limit("30/minute")
+def get_stats(request: Request, slug: str, db: Session = Depends(get_db)):
     entry = db.query(models.URL).filter(models.URL.slug==slug).first()
     if not entry:
         raise HTTPException(status_code=404, detail="Page not found (custom error)")
@@ -51,7 +59,8 @@ def get_stats(slug: str, db: Session = Depends(get_db)):
     return entry
 
 @app.get("/{slug}")
-def redirect(slug: str, db: Session = Depends(get_db)):
+@limiter.limit("60/minute")
+def redirect(request: Request, slug: str, db: Session = Depends(get_db)):
     entry = db.query(models.URL).filter(models.URL.slug==slug).first()
     if not entry:
         raise HTTPException(status_code=404, detail="Page not found")
